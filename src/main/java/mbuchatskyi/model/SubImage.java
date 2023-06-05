dbd05305dfb3f981e8adc8e554206ef71e9a6cc6
Maksym Buchatskyi <mbuchatskyi21@gmail.com>
2023-06-05 09:48:54 +0300

    Added documantation to the SubImage class

--- src/main/java/mbuchatskyi/model/SubImage.java
+++ src/main/java/mbuchatskyi/model/SubImage.java
@@ -9,118 +9,160 @@ import java.util.List;
 
 import javax.imageio.ImageIO;
 
+/**
+ * The instances of this class contains the pixels array of every border of the
+ * sub-image and sub-image itself.
+ */
 public class SubImage {
 	private List<Color> leftBorder = new ArrayList<>();
 	private List<Color> rightBorder = new ArrayList<>();
 	private List<Color> bottomBorder = new ArrayList<>();
 	private List<Color> topBorder = new ArrayList<>();
-	
+
 	private File file;
 	private BufferedImage image = new BufferedImage(320, 180, BufferedImage.TYPE_INT_ARGB);
-	
-	public SubImage(String file)  {
-	    this.file = new File(file);
-	    try {
+
+	/**
+	 * Constructor
+	 * 
+	 * @param file the file of the sub-image
+	 */
+	public SubImage(String file) {
+		this.file = new File(file);
+		try {
 			this.image = ImageIO.read(this.file);
 			init();
 		} catch (IOException e) {
 			System.out.println(e);
 		}
 	}
-	
+
+	/**
+	 * Calculates all pixels arrays
+	 */
 	private void init() {
 		calculateBottomBorderPixels();
 		calculateTopBorderPixels();
 		calculateLeftBorderPixels();
 		calculateRightBorderPixels();
 	}
-	
+
+	/**
+	 * Calculates pixel array of the left border
+	 */
 	private void calculateLeftBorderPixels() {
 		int height = image.getHeight();
-		
+
 		int[] borderPixels = new int[height];
 
 		for (int y = 0; y < height; y++) {
 			int rgb = image.getRGB(0, y);
 			borderPixels[y] = rgb;
 		}
-		
-        for (int y = 0; y < height; y++) {
-            Color color = new Color(borderPixels[y]);
-            leftBorder.add(color);
-        }
+
+		for (int y = 0; y < height; y++) {
+			Color color = new Color(borderPixels[y]);
+			leftBorder.add(color);
+		}
 	}
 
+	/**
+	 * Calculates pixel array of the right border
+	 */
 	private void calculateRightBorderPixels() {
 		int height = image.getHeight();
 		int width = image.getWidth();
-		
+
 		int[] borderPixels = new int[height];
 
 		for (int y = 0; y < height; y++) {
 			int rgb = image.getRGB(width - 1, y);
 			borderPixels[y] = rgb;
 		}
-		
-        for (int y = 0; y < height; y++) {
-            Color color = new Color(borderPixels[y]);
-            rightBorder.add(color);
-        }
+
+		for (int y = 0; y < height; y++) {
+			Color color = new Color(borderPixels[y]);
+			rightBorder.add(color);
+		}
 	}
-	
+
+	/**
+	 * Calculates pixel array of the bottom border
+	 */
 	private void calculateBottomBorderPixels() {
 		int height = image.getHeight();
 		int width = image.getWidth();
-		
+
 		int[] borderPixels = new int[width];
 
 		for (int x = 0; x < height; x++) {
 			borderPixels[x] = image.getRGB(x, height - 1);
 		}
-		
-        for (int y = 0; y < height; y++) {
-            Color color = new Color(borderPixels[y]);
-            bottomBorder.add(color);
-        }
+
+		for (int y = 0; y < height; y++) {
+			Color color = new Color(borderPixels[y]);
+			bottomBorder.add(color);
+		}
 	}
-	
+
+	/**
+	 * Calculates pixel array of the top border
+	 */
 	private void calculateTopBorderPixels() {
 		int height = image.getHeight();
 		int width = image.getWidth();
-		
+
 		int[] borderPixels = new int[width];
 
 		for (int x = 0; x < height; x++) {
 			borderPixels[x] = image.getRGB(x, 0);
 		}
-		
-        for (int y = 0; y < height; y++) {
-            Color color = new Color(borderPixels[y]);
-            topBorder.add(color);
-        }
+
+		for (int y = 0; y < height; y++) {
+			Color color = new Color(borderPixels[y]);
+			topBorder.add(color);
+		}
 	}
-	
+
+	/**
+	 * @return pixel array of the left border
+	 */
 	public List<Color> getLeftBorder() {
 		return leftBorder;
 	}
 
+	/**
+	 * @return pixel array of the right border
+	 */
 	public List<Color> getRightBorder() {
 		return rightBorder;
 	}
 
+	/**
+	 * @return pixel array of the bottom border
+	 */
 	public List<Color> getBottomBorder() {
 		return bottomBorder;
 	}
 
+	/**
+	 * @return pixel array of the top border
+	 */
 	public List<Color> getTopBorder() {
 		return topBorder;
 	}
 
+	/**
+	 * @return file of the sub-image
+	 */
 	public File getFile() {
 		return file;
 	}
 
+	/**
+	 * @return sub-image itself
+	 */
 	public BufferedImage getImage() {
 		return image;
-	}	
+	}
 }
